390172d5f92896f7b55701417ba0a5072fee6948
ImClean <ng.hoangson93@gmail.com>
2025-02-04 17:31:05 +0700

    Springboot security bang tay

--- 14-Bao-Mat-Spring-Boot/codetay/codetay-Springboot-Security/src/main/java/com/codetayspringbootsecurity/config/SecurityConfig.java
+++ 14-Bao-Mat-Spring-Boot/codetay/codetay-Springboot-Security/src/main/java/com/codetayspringbootsecurity/config/SecurityConfig.java
@@ -5,38 +5,41 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecutiryConfig {
+public class SecurityConfig {
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return NoOpPasswordEncoder.getInstance();
     }
 
     @Bean
     public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
-        UserDetailsManager user = User.withUsername("user").password(passwordEncoder().encode("user")).roles("USER").build();
-        UserDetailsManager admin = User.withUsername("admin").password(passwordEncoder().encode("admin")).roles("ADMIN").build();
+        UserDetails user = User.withUsername("user").password(passwordEncoder().encode("user")).roles("USER").build();
+        UserDetails admin = User.withUsername("admin").password(passwordEncoder().encode("admin")).roles("ADMIN").build();
         return new InMemoryUserDetailsManager(user, admin);
     }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.
-                authorizeHttpRequests(
-                        auth -> auth
-                                .requestMatchers("/admin/**").hasRole("ADMIN")
-                                .requestMatchers("/user/**").hasRole("USER")
-                                .anyRequest().authenticated()
-                )
+        http
                 .formLogin(Customizer.withDefaults())
+                .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> authorizationManagerRequestMatcherRegistry
+                        .anyRequest().authenticated()
+                )
                 .csrf(AbstractHttpConfigurer::disable);
         return http.build();
     }
